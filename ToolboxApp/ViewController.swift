cb4756655f7c83313a4c0e5ccb5c30b3ccb93679
Laura Sarmiento <lsarmientoal@gmail.com>
2019-10-12 19:10:22 -0500

    Initial Commit

---
+++ ToolboxApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToolboxApp
+//
+//  Created by Laura Sarmiento Almanza on 10/12/19.
+//  Copyright © 2019 Laura Sarmiento Almanza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToolboxAppTests/ToolboxAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  ToolboxAppTests.swift
+//  ToolboxAppTests
+//
+//  Created by Laura Sarmiento Almanza on 10/12/19.
+//  Copyright © 2019 Laura Sarmiento Almanza. All rights reserved.
+//
+
+import XCTest
+@testable import ToolboxApp
+
+class ToolboxAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
